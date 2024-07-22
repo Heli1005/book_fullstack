40ff7f63787ea0c2a0ce4e1a822cecb741ad41c7
Heli1005 <hgangwal4833@gmail.com>
2024-07-22 21:30:32 +0530

    change port of frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,11 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5173, // Port for the frontend
+    host: '0.0.0.0', // Bind to all network interfaces
     proxy: {
       '/api': {
         target: 'http://localhost:5000', // Your backend server URL
         port: 5000,
-        host: '0.0.0.0',
       },
     },
   },

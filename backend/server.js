a47ce72407f13cdb89887caa85f04d0db67021ae
Heli1005 <hgangwal4833@gmail.com>
2024-07-23 09:34:30 +0530

    change in mongodb uri

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000
 const MONGODB_URI = process.env.MONGODB_URI
 
 console.log('MongoDB URI:', MONGODB_URI); // Log to debug
-console.log('PORT:', process.env.PORT); // Log to debug
+console.log('PORT:==>', process.env.PORT); // Log to debug
 
 app.use(cors());
 app.use(express.json())

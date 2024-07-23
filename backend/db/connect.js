7ed37b1796ed6a6fcc1cc766da7284ee8bd62097
Heli1005 <hgangwal4833@gmail.com>
2024-07-23 08:54:07 +0530

    mongodb uri issue

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,14 +1,7 @@
 import mongoose from "mongoose";
 
-// console.log("uri", uri);
-
 const connect = (uri) => {
     const mongoDB_uri = process.env.MONGODB_URI
-    console.log("uri", mongoDB_uri);
-    
-    console.log("process.env.MONGODB_URI", process.env.MONGODB_URI);
-    
-    console.log("Connect with DB", mongoDB_uri);
-    return mongoose.connect(uri)
+    return mongoose.connect(mongoDB_uri)
 }
 export default connect
